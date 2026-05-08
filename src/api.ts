3a9450b0fdb7573720a948bb0100ce69a80cc463
BlaqOswald <askelshafik@gmail.com>
2026-05-08 18:03:17 +0300

    frontend ready

--- src/api.ts
+++ src/api.ts
@@ -6,9 +6,11 @@ const API = axios.create({
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-  if (token) {
+
+  if (token && req.headers) {
     req.headers.Authorization = `Bearer ${token}`;
   }
+
   return req;
 });
 
