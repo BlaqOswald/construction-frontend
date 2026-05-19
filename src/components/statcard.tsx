965aa2f1bf32e8570b674a8215dbfb2904b42643
BlaqOswald <askelshafik@gmail.com>
2026-05-19 16:08:00 +0300

    fix

--- src/components/statcard.tsx
+++ src/components/statcard.tsx
@@ -1,11 +1,19 @@
 export default function StatCard({ title, value, color }: any) {
   return (
     <div
-      className="bg-white p-5 rounded-xl shadow-sm border-l-4 hover:shadow-md transition"
+      className="
+        bg-white p-4 sm:p-5
+        rounded-xl shadow-sm border-l-4
+        hover:shadow-md transition
+        w-full
+      "
       style={{ borderColor: color }}
     >
-      <h4 className="text-gray-500 text-sm">{title}</h4>
-      <h2 className="text-2xl font-bold mt-2 text-gray-800">
+      <h4 className="text-gray-500 text-xs sm:text-sm">
+        {title}
+      </h4>
+
+      <h2 className="text-xl sm:text-2xl font-bold mt-1 sm:mt-2 text-gray-800">
         {value}
       </h2>
     </div>
